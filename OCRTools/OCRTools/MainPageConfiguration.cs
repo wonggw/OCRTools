fc5e35f9ad17f32b39cbcb216c06590a66ab92de
wong.guowei <wong.guowei@stengg.com>
2020-12-27 18:21:45 +0800

    Added first version of OCR Tools. Able to parse PDF and convert it into raw text.

---
+++ OCRTools/OCRTools/MainPageConfiguration.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+
+namespace OCRTools
+{
+    public partial class MainPage : Page
+    {
+        public const string FEATURE_NAME = "OCR";
+
+        List<Scenario> scenarios = new List<Scenario>
+        {
+            new Scenario() { Title="Receipts", ClassType=typeof(ReceiptScenario)},
+        };
+    }
+
+    public class Scenario
+    {
+        public string Title { get; set; }
+        public Type ClassType { get; set; }
+    }
+}
